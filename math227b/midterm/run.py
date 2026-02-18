0ebf0d3d2235cb7e77bd9afc0386f9e5f75f23ce
Karthik Desingu <56585697+karthik-d@users.noreply.github.com>
2026-02-18 08:06:21 -0800

    improve h ranges

--- math227b/midterm/run.py
+++ math227b/midterm/run.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from pkg.rk4.rk4 import rk4_solver
 from pkg.rk4.utils import plot_solutions
-from analysis.basic import test_function, run_order_analysis
+from analysis.basic import test_function, run_order_analysis_clean, run_order_analysis_regions
 
 
 if __name__=='__main__':
@@ -29,6 +29,13 @@ if __name__=='__main__':
 
 	
 	## 2. SYSTEMATIC TEST.
-	h_values = [0.2, 0.1, 0.05, 0.025]
-	errors, p_values = run_order_analysis(f, y, params, h_values)
+	# h_values = np.logspace(-4, 0, 50)
+	# errors, p_values = run_order_analysis(f, y, params, h_values)
+
+	# Clean convergence analysis
+	h_vals, errors, p_vals = run_order_analysis_clean(f, y, params)
+
+	# Region analysis
+	h_vals2, errors2, p_vals2, good_region = run_order_analysis_regions(f, y, params)
+
 
