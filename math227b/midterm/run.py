af3b78f672fd57b61a892c84985b2a34c97df2fd
Karthik Desingu <56585697+karthik-d@users.noreply.github.com>
2026-02-19 15:16:44 -0800

    restruct

--- math227b/midterm/run.py
+++ math227b/midterm/run.py
@@ -5,7 +5,7 @@ from pkg.descent.utils import check_hessian_pd
 from pkg.rk4.rk4 import rk4_solver
 from pkg.rk4.utils import plot_solutions
 
-from analysis.basic import test_function, run_order_analysis_clean, run_order_analysis_regions
+from analysis.rk4 import test_function, run_order_analysis_clean, run_order_analysis_regions
 from analysis.descent import rosenbrock_functions
 
 
@@ -33,36 +33,38 @@ if __name__=='__main__':
 
 	
 	## 2. SYSTEMATIC TEST: RK4.
-	# h_values = np.logspace(-4, 0, 50)
-	# errors, p_values = run_order_analysis(f, y, params, h_values)
+	# convergence analysis
+	h_vals, errors, p_vals = run_order_analysis_clean(f, y, params)
 
-	# Clean convergence analysis
-	# h_vals, errors, p_vals = run_order_analysis_clean(f, y, params)
-
-	# # Region analysis
-	# h_vals2, errors2, p_vals2, good_region = run_order_analysis_regions(f, y, params)
+	# Region analysis
+	h_vals2, errors2, p_vals2, good_region = run_order_analysis_regions(f, y, params)
 
 	
 	## 3. BASIC TEST: STEEPEST DESCENT.
-	f, grad, hess = rosenbrock_functions()
+	# f, grad, hess = rosenbrock_functions()
 
-	# Check Hessian at minimizer
-	x_star = np.array([1.0, 1.0])
-	H, eigvals = check_hessian_pd(hess, x_star)
+	# # Check Hessian at minimizer
+	# x_star = np.array([1.0, 1.0])
+	# H, eigvals = check_hessian_pd(hess, x_star)
 
-	print("Hessian at (1,1):")
-	print(H)
-	print("Eigenvalues:", eigvals)
+	# print("Hessian at (1,1):")
+	# print(H)
+	# print("Eigenvalues:", eigvals)
 
-	# Initial conditions
-	x0_list = [np.array([1.2, 1.2]), np.array([-1.2, 1.0])]
+	# # Initial conditions
+	# x0_list = [np.array([1.2, 1.2]), np.array([-1.2, 1.0])]
 
-	for x0 in x0_list:
-		print("\nInitial guess:", x0)
-		xmin, path = steepest_descent(f, grad, x0)
-		print("Computed minimizer:", xmin)
-		print("Function value:", f(xmin))
-		print("Gradient norm:", np.linalg.norm(grad(xmin)))
+	# for x0 in x0_list:
+	# 	print("\nInitial guess:", x0)
+	# 	xmin, path = steepest_descent(f, grad, x0)
+	# 	print("Computed minimizer:", xmin)
+	# 	print("Function value:", f(xmin))
+	# 	print("Gradient norm:", np.linalg.norm(grad(xmin)))
+
+
+	# show the converging points.
+	# plot error against iteraitons?
+	# viz. positive semi-definite somehow.
 
 
 
