fix error scaling at last timepoint

math227b/midterm/pkg/rk4/rk4.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def rk4_solver(f, t0, y0, h, tf):
     """
-    Classical 4th-order Runge-Kutta method.
+    Classical 4th-order Runge-Kutta method with fixed final time.
 
     Parameters
     ----------
@@ -24,19 +24,24 @@ def rk4_solver(f, t0, y0, h, tf):
     y : ndarray
         Numerical solution
     """
-    t = np.arange(t0, tf + h, h)
-    y = np.zeros_like(t)
+
+    # Number of steps (must be integer)
+    N = int(np.round((tf - t0) / h))
+    h = (tf - t0) / N   # enforce exact landing at tf
+
+    t = t0 + np.arange(N+1) * h
+    y = np.zeros((N+1,) + np.shape(y0), dtype=float)
     y[0] = y0
 
-    for i in range(len(t) - 1):
+    for i in range(N):
         ti = t[i]
         wi = y[i]
 
-        k1 = h * f(ti, wi)
-        k2 = h * f(ti + 0.5*h, wi + 0.5*k1)
-        k3 = h * f(ti + 0.5*h, wi + 0.5*k2)
-        k4 = h * f(ti + h, wi + k3)
+        k1 = f(ti, wi)
+        k2 = f(ti + 0.5*h, wi + 0.5*h*k1)
+        k3 = f(ti + 0.5*h, wi + 0.5*h*k2)
+        k4 = f(ti + h, wi + h*k3)
 
-        y[i+1] = wi + (k1 + 2*k2 + 2*k3 + k4) / 6.0
+        y[i+1] = wi + (h/6.0) * (k1 + 2*k2 + 2*k3 + k4)
 
     return t, y

math227b/midterm/run.py
@@ -5,42 +5,30 @@ from pkg.descent import utils as descent_utils
 from pkg.rk4.rk4 import rk4_solver
 from pkg.rk4 import utils as rk4_utils
 
-from analysis.rk4 import test_function, run_order_analysis_clean, run_order_analysis_regions
+from analysis.rk4 import test_function
 from analysis.descent import rosenbrock_functions, f1_shifted_quadratic, f2_mixed_poly_exp
 
 
 if __name__=='__main__':
 
-	# unload test function.
-	# f, y, params = test_function()
-
-	# ## 1. BASIC TEST: RK4.
-	# h=0.1
-	# # run solver.
-	# t_rk, y_rk = rk4_solver(f, params['t0'], params['y0'], h, params['tf'])
-
-	# # analytical solution.
-	# t_ex = np.linspace(params['t0'], params['tf'], 400)
-	# y_ex = y(t_ex)
-
-	# # plot.
-	# plot_solutions(t_rk, y_rk, t_ex, y_ex)
-
-	# # Error at t = 4
-	# y_exact_tf = y(params['tf'])
-	# rk_error = abs(y_rk[-1] - y_exact_tf)
-
+	## 1A. GIVEN FUNCTIION TESTS
 	f = lambda t, y: y - t**2 + 1
 	y_exact = lambda t: (t + 1)**2 - 0.5*np.exp(t)
 	t0, y0, tf = 0.0, 0.5, 4.0
 	h_values = [0.2, 0.1, 0.05, 0.025, 0.0125]
-	rk4_utils.plot_rk4_solutions(f, y_exact, t0, y0, tf, h_values)
-	rk4_utils.plot_rk4_relative_error(f, y_exact, t0, y0, tf, h_values)
-	rk4_utils.plot_rk4_local_error(f, y_exact, t0, y0, tf, h_values)
+	# rk4_utils.plot_rk4_solutions(f, y_exact, t0, y0, tf, h_values)
+	# rk4_utils.plot_rk4_relative_error(f, y_exact, t0, y0, tf, h_values)
+	# rk4_utils.plot_rk4_local_error(f, y_exact, t0, y0, tf, h_values)
 
-	h_values = np.logspace(0, -4, 500)
-	rk4_utils.rk4_relative_error_heatmap(f, y_exact, t0, y0, tf, h_values)
+	# h_values = np.logspace(0, -4, 500)
+	# rk4_utils.rk4_relative_error_heatmap(f, y_exact, t0, y0, tf, h_values)
 
+
+	## 1B. OTHER FUNCTION TESTS
+	h_values = np.logspace(-5, 0, 100)
+	local_errors, global_errors = rk4_utils.plot_loglog_error_with_slope(f, y_exact, t0, y0, tf, h_values)
+	rk4_utils.plot_piecewise_order(h_values, local_errors, global_errors)
+	# rk4_utils.plot_roundoff_vs_truncation(f, y_exact, t0, y0, tf)
 	
 	# 2. SYSTEMATIC TEST: RK4.
 	# convergence analysis